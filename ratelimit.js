4a19ed50d0deac39b5a387e25438102cddb76eea
itsphatto <lovephatto@gmail.com>
2026-09-13 02:57:45 +0700

    fix rate limit

--- ratelimit.js
+++ ratelimit.js
@@ -2,7 +2,7 @@ const ratelimit = require('express-rate-limit');
 
 const limiter = ratelimit({
     windowMs: 15 * 60 * 1000,
-    max: 3, 
+    max: 50, 
     message: 'Too many requests, please try again later.',
 });
 
