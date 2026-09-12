e035f6b1a3d3711ce4454221d4e5cd7c295efb0a
itsphatto <lovephatto@gmail.com>
2026-09-12 23:38:06 +0700

    Remove node_modules from tracking

---
+++ ratelimit.js
@@ -0,0 +1,10 @@
+const ratelimit = require('express-rate-limit');
+
+const limiter = ratelimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100, 
+    message: 'Too many requests, please try again later.',
+});
+
+app.use(limiter);
+app.use('/api/', limiter);
